58d68fd92f7df776b19ea329104387b5e6f5e336
劉謹逢 <47555881+lioujinfong@users.noreply.github.com>
2024-04-17 11:51:07 +0800

    Update Login_script.js

--- webGame/Login_script.js
+++ webGame/Login_script.js
@@ -1,26 +1,25 @@
-function sendData() {
+
+
+let users = [];
+
+// 加载 JSON 数据
+fetch('users.json')
+.then(response => response.json())
+.then(data => {
+    users = data;
+    console.log('Users loaded:', users);
+})
+.catch(error => console.error('Error loading JSON:', error));
+
+
+function checkEmail() {
     event.preventDefault(); // 阻止表單提交
     const email = document.getElementById('email').value;
-    fetch('http://120.125.73.101/~05170091/webGame/receive.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `email=${encodeURIComponent(email)}`
-    })
-    .then(response => response.text())
-    .then(data => {
-        if(data === "True"){
-          // alert("True"); // 或者用彈窗顯示服務器的響應
-          localStorage.setItem('email', email);
-          window.location.href = "./loginSuccess.html";
-        } else{
-          alert("Please check your email or password！"); // 或者用彈窗顯示服務器的響應
-        }
-        
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Error sending data'); // 顯示錯誤信息
-    });
+    const exists = users.some(user => user.email === email);
+    if (exists) {
+        // alert('Email exists!');
+        window.location.href = "./loginSuccess.html";
+    } else {
+        alert('Email does not exist. Please check your email or password!');
+    }
 }
