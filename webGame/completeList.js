cafd83c2463a2b7e9f0904dfc5a56a5df68ece37
劉謹逢 <47555881+lioujinfong@users.noreply.github.com>
2024-06-26 16:40:15 +0800

    Add files via upload

---
+++ webGame/completeList.js
@@ -0,0 +1,30 @@
+
+
+
+// 這邊只添加未完成項目
+document.addEventListener('DOMContentLoaded', function() {
+    const userDataStr = localStorage.getItem('user');
+    const userData = JSON.parse(userDataStr);
+    const completeIds = new Set(userData.completeId);
+    
+    const range = JSON.parse(localStorage.getItem('range')); // 取得範圍
+
+    fetch('./exhibitionList.json')
+    .then(response => response.json())
+    .then(data => {
+        const completeBody = document.getElementById('complete-table').getElementsByTagName('tbody')[0];
+        data.forEach(item => {
+            if (item.id >= range.min && item.id <= range.max && completeIds.has(item.id)) {
+                const row = document.createElement('tr');
+                row.insertCell(0).textContent = item.id;
+                row.insertCell(1).textContent = item.type;
+                row.insertCell(2).textContent = item.school;
+                row.insertCell(3).textContent = item.plan;
+                completeBody.appendChild(row);
+            }
+            
+
+        });
+    })
+    .catch(error => console.error('Error loading the data:', error));
+});
